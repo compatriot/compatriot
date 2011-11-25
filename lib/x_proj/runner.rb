88f10a09ffc574cc85a064a773e9f037a690723f
Carol Nichols <carol.nichols@gmail.com>
2011-11-25 17:23:54 -0500

    Make a directory for the screenshots based on the date and browsers

--- lib/x_proj/runner.rb
+++ lib/x_proj/runner.rb
@@ -1,4 +1,25 @@
+require "fileutils"
+
 module XProj
   class Runner
+    BROWSERS = ["firefox", "chrome"]
+
+    def initialize(clock = DateTime)
+      @clock = clock
+    end
+
+    def start
+      make_results_directory
+    end
+
+    private
+
+    def make_results_directory
+      timestamp = @clock.now.strftime("%Y-%m-%d-%H-%M-%S")
+      FileUtils.mkdir_p("tmp/results/#{timestamp}")
+      BROWSERS.each do |b|
+        FileUtils.mkdir_p("tmp/results/#{timestamp}/#{b}")
+      end
+    end
   end
 end
--- spec/basic_behavior_spec.rb
+++ spec/basic_behavior_spec.rb
@@ -1,30 +1,31 @@
 require_relative 'spec_helper'
+require 'fileutils'
+require 'date'
 
 describe "The very basics" do
   before do
     @current_dir = Dir.getwd
     Dir.chdir(File.join(File.dirname(__FILE__), 'sample_app'))
+    FileUtils.rmdir(File.join("tmp", "results"))
   end
 
   after do
     Dir.chdir(@current_dir)
   end
 
-  it "visits an app's homepage using selenium and takes screenshots" do
-    @x_proj = XProj::Runner.new
+  it "creates directories in which to store the results based on the date" do
+    fixed_clock = OpenStruct.new(:now => DateTime.parse("2012-01-01 00:00:00 UTC"))
+
+    @x_proj = XProj::Runner.new(fixed_clock)
     @x_proj.start
 
-    assert File.exists?("x_proj")
+    results_directory = File.join("tmp", "results", "2012-01-01-00-00-00")
+    assert File.exists?(results_directory)
 
-    firefox_directory = File.join("x_proj", "firefox")
-    chrome_directory  = File.join("x_proj", "chrome")
+    firefox_directory = File.join(results_directory, "firefox")
+    chrome_directory  = File.join(results_directory, "chrome")
 
     assert File.exists?(firefox_directory)
     assert File.exists?(chrome_directory)
-
-    Dir.entries(firefox_directory).size.must_equal(3)
-    Dir.entries(chrome_directory).size.must_equal(3)
-
-#    @driver = Capybara::Session.new(:selenium, TestApp).driver
   end
 end
