fac5a2441d211f617dc11a121938409744907acc
Carol Nichols <carol.nichols@gmail.com>
2011-11-27 20:14:24 -0500

    Refactor Runner#new and Runner#start into class method #start, not sure if I like this or not

--- lib/x_proj/runner.rb
+++ lib/x_proj/runner.rb
@@ -2,33 +2,38 @@ require "fileutils"
 
 module XProj
   class Runner
+    def self.start(app, clock = DateTime)
+      runner = new(app, clock)
+      runner.take_screenshots
+      runner.make_index_page
+    end
+
     BROWSERS = ["firefox", "chrome"]
 
-    def initialize(app, clock = DateTime)
+    attr_reader :app
+
+    def initialize(app, clock)
       @app   = app
       @clock = clock
     end
 
-    def start
-      @results_directory = make_results_directory
+    def take_screenshots
       BROWSERS.each do |b|
-        browser = XProj::Browser.new(b, @app, @results_directory)
+        browser = XProj::Browser.new(b, @app, results_directory)
         browser.start
       end
-      make_index_page
     end
 
-    private
-
-    def make_results_directory
+    def results_directory
+      return @results_directory if @results_directory
       timestamp = @clock.now.strftime("%Y-%m-%d-%H-%M-%S")
       directory_name = "tmp/results/#{timestamp}"
       FileUtils.mkdir_p(directory_name)
-      directory_name
+      @results_directory = directory_name
     end
 
     def make_index_page
-      index_page_file = File.join(@results_directory, "index.html")
+      index_page_file = File.join(results_directory, "index.html")
       html = "<html>
                 <head>
                   <title>Cross browser test results</title>
@@ -53,7 +58,7 @@ module XProj
 
       html << "</tr>"
 
-      screenshots = Dir.entries(File.join(@results_directory, BROWSERS.first))
+      screenshots = Dir.entries(File.join(results_directory, BROWSERS.first))
 
       screenshots = screenshots - [".", ".."]
 
--- spec/basic_behavior_spec.rb
+++ spec/basic_behavior_spec.rb
@@ -6,14 +6,11 @@ describe "The very basics" do
   before do
     @current_dir = Dir.getwd
     Dir.chdir(File.join(File.dirname(__FILE__), 'sample_app'))
-    FileUtils.remove_dir(File.join("tmp", "results"))
+    FileUtils.remove_dir(File.join("tmp", "results"), true)
 
     fake_date = DateTime.parse("2012-01-01 00:00:00 UTC")
     fake_date_dir = fake_date.strftime("%Y-%m-%d-%H-%M-%S")
-
-    fixed_clock = OpenStruct.new(:now => fake_date)
-    @x_proj = XProj::Runner.new(TestApp, fixed_clock)
-    @x_proj.start
+    @fixed_clock = OpenStruct.new(:now => fake_date)
 
     @results_directory = File.join("tmp", "results", fake_date_dir)
     @firefox_directory = File.join(@results_directory, "firefox")
@@ -24,19 +21,34 @@ describe "The very basics" do
     Dir.chdir(@current_dir)
   end
 
-  it "creates directories in which to store the results based on the date" do
-    assert File.exists?(@results_directory)
-    assert File.exists?(@firefox_directory)
-    assert File.exists?(@chrome_directory)
+  it "holds onto the app we give it" do
+    x = XProj::Runner.new(TestApp, @fixed_clock)
+    x.app.must_equal(TestApp)
   end
 
-  it "visits the home page and takes 1 screenshot per browser" do
-    Dir.glob(File.join(@firefox_directory, "*.png")).size.must_equal 1
-    Dir.glob(File.join(@chrome_directory, "*.png")).size.must_equal 1
+  it "names a results directory based on the clock" do
+    x = XProj::Runner.new(TestApp, @fixed_clock)
+    x.results_directory.must_equal(@results_directory)
   end
 
-  it "creates an index page that shows the screenshots" do
-    assert File.exists?(File.join(@results_directory, "index.html"))
+  describe "self#start" do
+    before do
+      XProj::Runner.start(TestApp, @fixed_clock)
+    end
 
+    it "creates directories in which to store the results based on the date" do
+      assert File.exists?(@results_directory)
+      assert File.exists?(@firefox_directory)
+      assert File.exists?(@chrome_directory)
+    end
+
+    it "visits the home page and takes 1 screenshot per browser" do
+      Dir.glob(File.join(@firefox_directory, "*.png")).size.must_equal 1
+      Dir.glob(File.join(@chrome_directory, "*.png")).size.must_equal 1
+    end
+
+    it "creates an index page that shows the screenshots" do
+      assert File.exists?(File.join(@results_directory, "index.html"))
+    end
   end
 end
