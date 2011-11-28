ac3bc10558a748b50d0869462e0c6bc871a552b2
Carol Nichols <carol.nichols@gmail.com>
2011-11-27 20:15:25 -0500

    Refactor Browser to not do as much work in the initialize method

--- lib/x_proj/browser.rb
+++ lib/x_proj/browser.rb
@@ -8,10 +8,7 @@ module XProj
     def initialize(browser_name, app, results_directory)
       @browser_name        = browser_name
       @browser_selenium_id = translate_to_selenium(@browser_name)
-      @screenshot_dir      = "#{results_directory}/#{@browser_name}"
-
-      FileUtils.mkdir_p(@screenshot_dir)
-
+      @results_directory   = results_directory
       @app = app
     end
 
@@ -25,8 +22,15 @@ module XProj
 
       visit "/"
       file_base_name = "1.png"
-      screenshot_path = File.join(@screenshot_dir, file_base_name)
-      Capybara.page.driver.browser.save_screenshot(screenshot_path)
+      filepath = File.join(screenshot_path, file_base_name)
+      Capybara.page.driver.browser.save_screenshot(filepath)
+    end
+
+    def screenshot_path
+      return @screenshot_path if @screenshot_path
+      @screenshot_path = "#{@results_directory}/#{@browser_name}"
+      FileUtils.mkdir_p(@screenshot_path)
+      @screenshot_path
     end
 
     private
