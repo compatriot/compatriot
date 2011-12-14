bf01f96e746c78a0cac5712a84e4448409c268a4
Carol Nichols <carol.nichols@gmail.com>
2011-12-13 20:42:48 -0500

    OH HEY! These unit tests sure are a pain, lots of mocking... wonder if that means anything about my design?!?! ;)

--- spec/unit/runner_spec.rb
+++ spec/unit/runner_spec.rb
@@ -7,11 +7,57 @@ describe Compatriot::Runner do
     @fixed_clock   = OpenStruct.new(:now => fake_date)
   end
 
-  it "names a results directory in tmp/results based on the clock" do
-    results_dir_name = File.join("tmp", "results", @fake_date_dir)
-    x = Compatriot::Runner.new(TestApp, ["/"], @fixed_clock)
+  describe "self#start" do
+    it "creates a Runner instance and calls the other methods on it" do
+      runner = stub
 
-    x.results_directory.must_equal(results_dir_name)
+      Compatriot::Runner.expects(:new).returns(runner)
+      runner.expects(:create_results_directory)
+      runner.expects(:take_screenshots)
+      runner.expects(:compute_diffs)
+      runner.expects(:make_index_page)
+
+      Compatriot::Runner.start(stub, stub)
+    end
+  end
+
+  describe "#take_screenshots" do
+    it "calls take_screenshots for each browser" do
+      results = stub
+      Compatriot::Results.stubs(:new).returns(results)
+
+      app = stub
+      paths = stub
+      dir = "somedate"
+
+      runner = Compatriot::Runner.new(app, paths, stub)
+      runner.stubs(:results_directory).returns(dir)
+
+      results.expects(:take_screenshots).with(
+        :browser => "firefox",
+        :app     => app,
+        :paths   => paths,
+        :results_directory => dir
+      )
+      results.expects(:take_screenshots).with(
+        :browser => "chrome",
+        :app     => app,
+        :paths   => paths,
+        :results_directory => dir
+      )
+
+      runner.take_screenshots
+    end
+  end
+
+
+  describe "#results_directory" do
+    it "names a results directory in tmp/results based on the clock" do
+      results_dir_name = File.join("tmp", "results", @fake_date_dir)
+      runner = Compatriot::Runner.new(TestApp, ["/"], @fixed_clock)
+
+      runner.results_directory.must_equal(results_dir_name)
+    end
   end
 
 end
