f99b4cd2029c6c51f4db688eea23dbb4dd8dbed7
Carol Nichols <carol.nichols@gmail.com>
2011-12-21 11:41:36 -0500

    Refactored away a file and forgot to remove it from the requires, bad carol

--- lib/compatriot.rb
+++ lib/compatriot.rb
@@ -2,7 +2,6 @@ require "compatriot/version"
 require "compatriot/runner"
 require "compatriot/browser"
 require "compatriot/results_presenter"
-require "compatriot/results"
 require "compatriot/image_differ"
 
 module Compatriot
