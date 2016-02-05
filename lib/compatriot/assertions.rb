8ffdf5f57a6ca51abaa354cab0856519ab639ae4
Jeff Koenig <jkoenig311@gmail.com>
2016-02-05 13:04:40 -0500

    This little change will save us some horizontal line space
    I just like this a little bit better.

--- lib/compatriot/assertions.rb
+++ lib/compatriot/assertions.rb
@@ -1,8 +1,6 @@
-module Compatriot
-  module Assertions
-    def assert_no_ui_changes(title = '')
-      diff = Compatriot.percentage_changed(self, title)
-      assert diff <= Compatriot.ui_difference_threshold, "The difference in the page (#{diff}%) is greater then the threshold #{Compatriot.ui_difference_threshold}"
-    end
+module Compatriot::Assertions
+  def assert_no_ui_changes(title = '')
+    diff = Compatriot.percentage_changed(self, title)
+    assert diff <= Compatriot.ui_difference_threshold, "The difference in the page (#{diff}%) is greater then the threshold #{Compatriot.ui_difference_threshold}"
   end
 end
