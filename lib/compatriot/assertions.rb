f8fedd01882a08de649038b7a1ebe7a923cbadec
Jeff Koenig <jkoenig311@gmail.com>
2016-05-03 15:22:25 -0400

    Add support for reporting on multiple assertions in a test

--- lib/compatriot/assertions.rb
+++ lib/compatriot/assertions.rb
@@ -3,6 +3,11 @@ require 'compatriot'
 module Compatriot
   module Assertions
     def assert_no_ui_changes(title = '')
+      class << self
+        attr_accessor :compatriot_assertion_titles
+      end
+      self.compatriot_assertion_titles ||= []
+      self.compatriot_assertion_titles << title
       diff = Compatriot.percentage_changed(self, title)
       diff_file = Compatriot.filepath_for_screenshot('diffs', Compatriot.filename_for_test(self, title))
       puts "% diff is #{diff}. #{diff_file}" if Compatriot.show_diffs
