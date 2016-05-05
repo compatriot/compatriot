ca28c3aca5dffc9b8b9c44d591b34df598c4db5c
Jeff Koenig <jkoenig311@gmail.com>
2016-05-05 15:46:30 -0400

    Handle a case where no assertion is made
    This can happen if the test fails before it gets to an assertion, if
    that happens, we should not break on report generation.

--- spec/unit/report_spec.rb
+++ spec/unit/report_spec.rb
@@ -23,5 +23,30 @@ describe Compatriot::Reporter do
     Compatriot::Reporter.new(tests).run
 
     assert File.exist?(REPORT_FILENAME), "#{REPORT_FILENAME} not found"
+    File.readlines(REPORT_FILENAME).grep(/label class='collapse/).count.must_equal 2
+  end
+
+  it 'generates a report even if there were not any assertions made' do
+    tests = [stub_everything('test',
+                            name: 'test_1_viewing_a_specific_report',
+                            location: 'Some description',
+                            compatriot_assertion_titles: [ ],
+                            passed?: true)
+            ]
+    Compatriot::Reporter.new(tests).run
+    assert File.exist?(REPORT_FILENAME), "#{REPORT_FILENAME} not found"
+    File.readlines(REPORT_FILENAME).grep(/label class='collapse/).count.must_equal 0
+  end
+
+  it 'handles multiple assertions in one test' do
+    tests = [stub_everything('test',
+                             name: 'test_1_viewing_a_specific_report',
+                             location: 'Some description',
+                             compatriot_assertion_titles: [ 'first assertion', 'second assertion' ],
+                             passed?: true)
+            ]
+    Compatriot::Reporter.new(tests).run
+    assert File.exist?(REPORT_FILENAME), "#{REPORT_FILENAME} not found"
+    File.readlines(REPORT_FILENAME).grep(/label class='collapse/).count.must_equal 2
   end
 end
