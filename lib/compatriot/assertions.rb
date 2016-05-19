3ebb7e56323f2c82f81c6726eb9dfd41e7716988
Jeff Koenig <jkoenig311@gmail.com>
2016-05-19 09:07:16 -0400

    If the first diff is too big, sleep and try once more
     We are going to allow 1 retry after failure
     because there is a weird case that keeps
     reoccurring that seems like the page
     might still be rendering. It looks like
     the font spacing is still adjusting slightly

--- lib/compatriot/assertions.rb
+++ lib/compatriot/assertions.rb
@@ -10,16 +10,26 @@ module Compatriot
       self.compatriot_assertion_titles ||= []
       self.compatriot_assertion_titles << title
       self.compatriot_percentages_changed ||= []
-      diff = Compatriot.percentage_changed(self, title)
-      self.compatriot_percentages_changed << diff
       diff_file = Compatriot.filepath_for_screenshot('diffs', Compatriot.filename_for_test(self, title))
+
+      diff = Compatriot.percentage_changed(self, title)
       puts "% diff is #{diff}. #{diff_file}" if Compatriot.show_diffs
       pass = diff <= Compatriot.ui_difference_threshold
 
+      # We are going to allow 1 retry after failure
+      # because there is a weird case that keeps
+      # reoccurring that seems like the page
+      # might still be rendering. It looks like
+      # the font spacing is still adjusting slightly
       unless pass
-        puts "Diff > ThresholdFailed: see #{diff_file}"
+        puts "Found a difference, retrying once"
+        sleep 1
+        diff = Compatriot.percentage_changed(self, title)
+        puts "% diff is #{diff}. #{diff_file}" if Compatriot.show_diffs
+        pass = diff <= Compatriot.ui_difference_threshold
       end
 
+      self.compatriot_percentages_changed << diff
       assert pass, "The difference in the page (#{diff}%) is greater then the threshold #{Compatriot.ui_difference_threshold}"
     end
   end
